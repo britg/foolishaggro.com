1262b5bb38c72d7681473638017e60a676fc4599
britg <brit@britg.com>
2013-08-20 13:18:51 -0500

    Update homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,8 @@ class PagesController < ActionController::Base
   layout "pages"
 
   def index
-
+    @admin_ids = User.where(:admin => true).map(&:id)
+    @topics = Topic.by_newest.where(:user_id => @admin_ids)
   end
 
   def game
