3f022bd07481a7822f475fbcbf6339f832a2d553
britg <brit@britg.com>
2013-08-21 19:43:19 -0500

    Update homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,12 @@ class PagesController < ActionController::Base
 
   def index
     @admin_ids = User.where(:admin => true).map(&:id)
-    @topics = Topic.by_newest.visible.where(:user_id => @admin_ids, :archetype => Archetype.default).limit(10)
+    @topics = Topic.by_newest
+                   .visible
+                   .where(:user_id => @admin_ids,
+                          :archetype => Archetype.default)
+                   .where("category_id not in (5)")
+                   .limit(10)
   end
 
   def game
