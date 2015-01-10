4547aa4f8683aa6199a830de49a05f91a3536d3e
britg <brit@britg.com>
2015-01-10 08:44:50 -0600

    Use controller base instead of application controller

--- app/controllers/gameapis_controller.rb
+++ app/controllers/gameapis_controller.rb
@@ -1,4 +1,6 @@
-class GameapisController < ApplicationController
+class GameapisController < ActionController::Base
+
+  layout nil
 
   def mazingdaily
     render text: Time.now.beginning_of_day.to_i
